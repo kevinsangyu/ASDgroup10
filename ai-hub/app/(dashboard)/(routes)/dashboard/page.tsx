bc87597ca5bd950ca0f64c805068909303a6d570
CharlieH7 <hope91824@gmail.com>
2023-09-12 00:44:19 +1000

    fixed landing page ui + completed side bar and dashboard with minor bugs fixes and improvement

--- ai-hub/app/(dashboard)/(routes)/dashboard/page.tsx
+++ ai-hub/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,14 +1,85 @@
-import { Button } from '@/components/ui/button'
-import { UserButton } from '@clerk/nextjs';
-import Image from 'next/image'
+"use client";
+
+import { useRouter } from "next/navigation";
+import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import { ArrowRight, Code, Image, MessageSquare, Music, Video } from "lucide-react";
+
+const tools = [
+  {
+    label: "Conversation",
+    icon: MessageSquare,
+    color: "text-violet-500",
+    bgColor: "bg-violet-500/10",
+    href: "/conversation"
+  },
+  {
+    label: "Code Generation",
+    icon: Code,
+    color: "text-blue-500",
+    bgColor: "bg-blue-500/10",
+    href: "/code"
+  },
+  {
+    label: "Image Generation",
+    icon: Image,
+    color: "text-pink-500",
+    bgColor: "bg-pink-500/10",
+    href: "/image"
+  },
+  {
+    label: "Video Generation",
+    icon: Video,
+    color: "text-green-500",
+    bgColor: "bg-green-500/10",
+    href: "/video"
+  },
+  {
+    label: "Music Generation",
+    icon: Music,
+    color: "text-orange-500",
+    bgColor: "bg-orange-500/10",
+    href: "/music"
+  },
+]
 
 const DashboardPage = () => {
+  const router = useRouter();
   return (
     <>
-    <p>Dashboard Page (Protected)</p>
-    <UserButton afterSignOutUrl='/'/>
+      <div className="mb-8 space-y-4">
+        <h2 className="text-2xl md:text-4xl font-bold text-center">
+          Welcome to AI Hub{' '}
+          <span className="animate-waving-hand">👋</span>
+        </h2>
+        <p className="text-muted-foreground font-light text-sm
+        md:text-lg text-center">
+          Play with the most comprehensive AI platform
+        </p>
+      </div>
+      <div className="px-4 md:px-20 lg:px-32 space-y-4">
+        {tools.map((tool) => (
+          <Card 
+           onClick={() => router.push(tool.href)}
+           key={tool.href}
+           className="p-4 border-black/5 flex items-center
+           justify-between hover:shadow-md transition
+           cursor-pointer"
+          >
+            <div className="flex items-center gap-x-4">
+              <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
+                <tool.icon className={cn("w-8 h-8", tool.color)}/>
+              </div>
+              <div className="font-semibold">
+                {tool.label}
+              </div>
+            </div>
+            <ArrowRight className="w-5 h-5"/>
+          </Card>
+        ))}
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default DashboardPage;
--- ai-hub/app/(landing)/page.tsx
+++ ai-hub/app/(landing)/page.tsx
@@ -1,26 +1,55 @@
-import { Button } from "@/components/ui/button";
-import Link  from "next/link";
+"use client";
 import React from "react";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 const LandingPage = () => {
   return (
-    <div>
-        Landing Page (Unprotected) 
-        <div>
+    <div className="landing-page">
+      <div className="content">
+        <h1>Welcome to AI Hub 👋</h1>
+        <p>Explore the power of AI</p>
+        <div className="buttons">
           <Link href="/sign-in">
-            <Button>
-              Login
-            </Button>
+            <Button>Login</Button>
           </Link>
           <Link href="/sign-up">
-            <Button>
-              Register
-            </Button>
+            <Button>Register</Button>
           </Link>
         </div>
-    </div>
-  )
+      </div>
 
+      <style jsx>{`
+        .landing-page {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          height: 100vh;
+        }
+
+        .content {
+          text-align: center;
+        }
+
+        h1 {
+          font-size: 2.5rem;
+          margin-bottom: 1rem;
+        }
+
+        p {
+          font-size: 1.2rem;
+          margin-bottom: 2rem;
+        }
+
+        .buttons {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          gap: 1rem;
+        }
+      `}</style>
+    </div>
+  );
 };
 
 export default LandingPage;
