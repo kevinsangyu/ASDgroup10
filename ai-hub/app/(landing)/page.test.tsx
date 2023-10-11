aece83c5ce0bf43b6063297338e1543973794001
CharlieH7 <hope91824@gmail.com>
2023-10-12 10:00:02 +1100

    added test file

--- ai-hub/app/(landing)/page.test.tsx
+++ ai-hub/app/(landing)/page.test.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Home from './page';
 
-
 // Mocking the next/link since it won't work outside of a Next.js app context.
-jest.mock('next/link', () => ({ children }: { children: React.ReactNode }) => children);
-
+jest.mock('next/link', () => {
+    return ({ children }: { children: React.ReactNode }) => children;
+});
+  
 describe('<Home />', () => {
 
   it('renders without crashing', () => {
