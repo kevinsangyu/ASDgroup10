54ffd5d0ad8d186a109701665ca1cb0dfa2e1f02
malcolm-mccredie <100190593+malcolm-mccredie@users.noreply.github.com>
2023-10-12 02:31:16 +1100

    music ai working
    got the music ai to work

--- ai-hub/app/(dashboard)/(routes)/music/page.tsx
+++ ai-hub/app/(dashboard)/(routes)/music/page.tsx
@@ -9,11 +9,16 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { Loader } from "@/components/loader";
+import { Empty } from "@/components/empty";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 import axios from "axios";
-//import { useRouter } from 'next/router';  NEED TO FIX USEROUTER AS IT ERRORS WITH NEXT ROUNTER NOT MOUNTED
 
 const MusicPage = () => {
- //   const router = useRouter();
+    const router = useRouter();
+    const [music, setMusic] = useState<string>();
+
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -24,64 +29,51 @@ const MusicPage = () => {
     const isLoading = form.formState.isSubmitting;
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        console.log(values);
         try {
-//            setMusic(undefined)
+            setMusic(undefined);
+            console.log(values);
 
             const response = await axios.post("/api/music", values);
-
-//            setMusic(response.data.audio);
-            form.reset();
+            setMusic(response.data.audio);
+            form.reset()
         } catch (error: any) {
             console.log(error);
         } finally {
- //           router.reload();
-        } 
-        
+            router.refresh()
+        }
     }
 
     return (
         <div>
-        <Heading 
-            title="Music Generation"
-            description="Enter a prompt to generate music!"
-            icon={Music}
-            iconColor="text-orange-500"
-            bgColor="bg-orange-500/10"
-        />
+
+            <Heading
+                title="Music generation"
+                description="Enter a prompt to generate music!"
+                icon={Music}
+                iconColor="text-orange-500"
+                bgColor="bg-orange-500/10"
+            />
+
             <div className="px-4 lg:px-8">
                 <div>
                     <Form {...form}>
                         <form
                             onSubmit={form.handleSubmit(onSubmit)}
-                            className="
-                            rounded-lg
-                            border
-                            w-full
-                            p-4
-                            px-3
-                            md:px-6
-                            focus-within:shadow-sm
-                            grid
-                            grid-cols-12
-                            gap-2
-                        "
-
-                        >
-                            <FormField 
-                            name="prompt"
-                            render={({ field }) => (
-                                <FormItem className="col-span-12 lg:col-span-10">
-                                    <FormControl className="m-0 p-0">
-                                        <Input 
-                                            className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
-                                            disabled={isLoading}
-                                            placeholder="Guitar solo with a violin"
-                                            {...field}
-                                        />
-                                    </FormControl>
-                                </FormItem>
-                            )}
+                            className="rounded-lg border w-full p-4 px-3 md:px-6 focus-within:shadow-sm grid grid-cols-12 gap-2">
+                            <FormField
+                                name="prompt"
+                                render={({ field }) => (
+                                    <FormItem className="col-span-12 lg:col-span-10">
+                                        <FormControl className="m-0 p-0">
+                                            <Input
+                                                className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                                                disabled={isLoading}
+                                                placeholder="90s hip hop"
+                                                {...field}
+                                            />
+                                        </FormControl>
+                                    </FormItem>
+                                )}
                             />
                             <Button className="col-span-12 lg:col-span-2 w-full" disabled={isLoading}>
                                 Generate
@@ -89,15 +81,25 @@ const MusicPage = () => {
                         </form>
                     </Form>
                 </div>
+                <div className="space-y-4 mt-4">
+                        {isLoading && (
+                            <div className="p-8
+                            rounded-lg w-full flex items-centre justify-centre bg-muted">
+                                <Loader />
+                                </div>
+                        )}
+                        {!music && !isLoading && (
+                            <Empty label="No music generated." />
+                        )}
+                        {music && (
+                            <audio controls className="w-full mt-8">
+                                <source src={music} type="audio/mpeg" />
+                            </audio>
+                    )}
+                    </div>
             </div>
         </div>
     );
 }
 
 export default MusicPage;
-
-/*
-function setMusic(undefined: undefined) {
-    throw new Error("Function not implemented.");
-}
-*/
