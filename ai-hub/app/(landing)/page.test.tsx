cd52ebd03cbf31e1fd43c35c589d95020b30b50a
CharlieH7 <hope91824@gmail.com>
2023-10-12 09:39:16 +1100

    added test file

---
+++ ai-hub/app/(landing)/page.test.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Home from './page';
+
+
+// Mocking the next/link since it won't work outside of a Next.js app context.
+jest.mock('next/link', () => ({ children }: { children: React.ReactNode }) => children);
+
+describe('<Home />', () => {
+
+  it('renders without crashing', () => {
+    const { container } = render(<Home />);
+    expect(container.firstChild).toBeDefined();
+  });
+
+  it('displays the Welcome header', () => {
+    render(<Home />);
+    expect(screen.getByText('Welcome to AI Hub 👋')).toBeDefined();
+  });
+
+  it('renders Login and Register buttons', () => {
+    render(<Home />);
+    expect(screen.getByText('Login')).toBeDefined();
+    expect(screen.getByText('Register')).toBeDefined();
+  });
+
+});
