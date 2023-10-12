a041f34b476e52e9a961db3efccb47136e6b787f
Kevin <kevin.sang.yu@gmail.com>
2023-10-13 10:12:21 +1100

    Installed dotenv to give the test file access to the process.env file so it can access the api keys. The test file works well

--- ai-hub/app/api/image/image.test.tsx
+++ ai-hub/app/api/image/image.test.tsx
@@ -1,3 +1,9 @@
+// this is so that the test environment has access to process.env file for the API keys.
+// it is before the imports because when importing POST it throws an error because it cant find the api keys in the .env file
+const path = require('path');
+
+require('dotenv').config({ path: path.resolve(__dirname, '../../../.env') });
+
 import { POST } from './route';
 import { NextApiRequest } from 'next';
 
@@ -13,14 +19,13 @@ const convertToRequest = (req: NextApiRequest): Request => {
 
 // test the image generation function to see if it accepts the given arguments and returns http status 200
 describe('POST function', () => {
-  it('successfully accepts the given arguments', async () => {
+  it('successfully accepts the given arguments and validates the status of the API key', async () => {
     // mock the auth function return a user ID
     jest.spyOn(require('@clerk/nextjs'), 'auth').mockReturnValueOnce({ userId: 'mockedUserId' });
 
-    // create a mock request object that emulates a POST request
+    // create a mock request object that emulates a POST request with the required arguments
     const mockReq: NextApiRequest = {
       method: 'POST',
-      body: JSON.stringify({ prompt: "A bowl of cookies", amount: 1, resolution: "256x256" }),
       json: jest.fn().mockResolvedValueOnce({ prompt: "A bowl of cookies", amount: 1, resolution: "256x256" }),
     } as any;
 
@@ -28,6 +33,8 @@ describe('POST function', () => {
     const response = await POST(convertToRequest(mockReq));
     
     // expect the response of the POST function to be http status 200 (ok)
+    // status 400 means there's a missing argument
+    // status 500 means bad authorization, which usually pertains to the api key being invalid
     expect(response.status).toBe(200);
 
   }, 12000); // it actually spends time to generate which takes ~9.5 seconds so give it 12000ms timeframe
