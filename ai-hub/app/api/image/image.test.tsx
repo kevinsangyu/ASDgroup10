dd5b2498958b93a9a12b9ed122702d31b2efd382
Kevin <kevin.sang.yu@gmail.com>
2023-10-13 09:59:10 +1100

    Redoing the image gen testing, as I realise the previous testing actually does nothing. This time I've got it set up to actually send a generation request to the API, and it just checks the response status is http status 200. However there's an issue where the test environment cant access the .env file to reach the api keys.

--- ai-hub/app/api/image/image.test.tsx
+++ ai-hub/app/api/image/image.test.tsx
@@ -1,16 +1,9 @@
 import { POST } from './route';
 import { NextApiRequest } from 'next';
 
-// mock the auth function
+// mock the auth function,as the test process will be conducted while not logged in
 jest.mock('@clerk/nextjs', () => ({
   auth: jest.fn(),
-}));
-
-// mock the OpenAI client
-jest.mock('openai', () => ({
-  OpenAI: jest.fn(() => ({
-    apiKey: 'mockedApiKey',
-  })),
 }));
 
 // creating generic request object from next's request class
@@ -18,24 +11,24 @@ const convertToRequest = (req: NextApiRequest): Request => {
   return req as unknown as Request;
 };
 
-// test the image generation function to see if it accepts the given arguments
+// test the image generation function to see if it accepts the given arguments and returns http status 200
 describe('POST function', () => {
   it('successfully accepts the given arguments', async () => {
     // mock the auth function return a user ID
     jest.spyOn(require('@clerk/nextjs'), 'auth').mockReturnValueOnce({ userId: 'mockedUserId' });
 
-    // create a mock request object that emulates a POST request with dummy response data
+    // create a mock request object that emulates a POST request
     const mockReq: NextApiRequest = {
       method: 'POST',
       body: JSON.stringify({ prompt: "A bowl of cookies", amount: 1, resolution: "256x256" }),
-      json: jest.fn().mockResolvedValueOnce({ response: [] }),
+      json: jest.fn().mockResolvedValueOnce({ prompt: "A bowl of cookies", amount: 1, resolution: "256x256" }),
     } as any;
 
     // call the POST function with the mock request
-    await POST(convertToRequest(mockReq));
+    const response = await POST(convertToRequest(mockReq));
+    
+    // expect the response of the POST function to be http status 200 (ok)
+    expect(response.status).toBe(200);
 
-    // check that the auth function was called
-    expect(require('@clerk/nextjs').auth).toHaveBeenCalled();
-
-  });
+  }, 12000); // it actually spends time to generate which takes ~9.5 seconds so give it 12000ms timeframe
 });
