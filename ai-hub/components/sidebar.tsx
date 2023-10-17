e35452f5d83d90db0571330a8db3471c8ae7441a
William Herman <william@s1t2.com>
2023-10-17 18:15:26 +1100

    implement pro modal subscription

--- ai-hub/components/sidebar.tsx
+++ ai-hub/components/sidebar.tsx
@@ -66,10 +66,12 @@ const routes = [
 
 interface SidebarProps {
   apiLimitCount: number;
+  isPro: boolean;
 };
 
 const Sidebar = ({
-  apiLimitCount = 0
+  apiLimitCount = 0,
+  isPro = false,
 }: SidebarProps) => {
   const pathname = usePathname();
   return (
@@ -104,6 +106,7 @@ const Sidebar = ({
         </div>
       </div>
       <FreeCounter 
+        isPro={isPro}
         apiLimitCount={apiLimitCount}
       />
     </div>
