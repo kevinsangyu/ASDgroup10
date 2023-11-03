5ad5d7f293bbbffdf14fb7762f6a6cd57699a6be
William Herman <williamhermann25@gmail.com>
2023-11-03 14:05:51 +1100

    refactor copyright design

--- ai-hub/app/(dashboard)/(routes)/dashboard/page.tsx
+++ ai-hub/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -109,10 +109,7 @@ const DashboardPage = () => {
               .footer {
                 position: absolute;
                 bottom: 0;
-                width: 100%;
                 text-align: center;
-                background-color: #f5f5f5; /* Optional background color */
-          
               }
       `}</style>
     </>
